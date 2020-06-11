e09b16b6d586f8f7bfd731e2451afae6cca86af9
kethan-sai <ykethansai@gmail.com>
2020-06-11 11:45:07 -0500

    Context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends React.Component {
   state = { language: "english" };
@@ -22,7 +24,12 @@ class App extends React.Component {
             onClick={() => this.onLanguageChange("dutch")}
           ></i>
         </div>
-        <UserCreate />
+
+        <ColorContext.Provider value="red">
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
